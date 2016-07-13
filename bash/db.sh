89230a66d47a9e1c3445872f794dc9f7664718fc
Shaohua Huang <hshnju@gmail.com>
2016-07-13 13:16:32 +0800

    fix

--- bash/db.sh
+++ bash/db.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-if psql mydb -c '\q' 2>&1;
+if /usr/local/bin/psql mydb -c '\q' 2>&1;
 then
     echo "mydb already exists"
 else
     echo "mydb not exists yet"
-    createdb mydb
+    /usr/local/bin/createdb mydb
 fi
